2d11233953aac27588a646a4774f8a0e0a81b329
DmitrySamsonov <dimasamsonov7@gmail.com>
2016-12-11 02:14:47 +0300

    Add Framework Support -> Spring(MVC,Security,WebFlow)

---
+++ src/main/java/servlets/ListServlet.java
@@ -0,0 +1,25 @@
+package servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by dima on 11.12.2016.
+ */
+
+public class ListServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+//        RequestDispatcher editView = request.getRequestDispatcher("second.jsp");
+//        editView.forward(request, response);
+    }
+}
