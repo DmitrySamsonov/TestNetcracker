02d7b5c8a99e3bd63b6ff829e73ca060d0228151
DmitrySamsonov <dimasamsonov7@gmail.com>
2016-12-14 02:23:27 +0300

    add StudentDAO

--- src/main/java/com/university/dao/impl/StudentDAOImpl.java
+++ src/main/java/com/university/dao/impl/StudentDAOImpl.java
@@ -1,46 +1,42 @@
-package com.university.dao.impl;//package com.university.com.university.dao.impl;
-//
-//import java.util.List;
-//
-//import org.hibernate.Criteria;
-//import org.hibernate.SessionFactory;
-//import org.springframework.com.university.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Component;
-//import org.springframework.stereotype.Repository;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import com.university.com.university.dao.interfaces.StudentDAO;
-//import com.university.com.university.entities.Student;
-//
-//
-//
-//@Repository
-//public class StudentDAOImpl implements StudentDAO {
-//
-//
-//
-//    @Autowired
-//    private SessionFactory sessionFactory;
-//
-//    private List<Student> students;
-//
-//
-//    @Transactional
-////    @Override
-//    public /*List<Student>*/void getStudents() {
-//
-//        System.out.println("sessionFactory = " + sessionFactory);
-//
-//        students = (List<Student>) sessionFactory.getCurrentSession()
-//                .createCriteria(Student.class)
-//                .setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY).list();
-//
-//        System.out.println("sessionFactory = " + sessionFactory);
-//
-////        return students;
-//    }
-//
-//
-//
-//}
+package com.university.dao.impl;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import com.university.dao.interfaces.StudentDAO;
+import com.university.entities.Student;
+
+
+@Component
+public class StudentDAOImpl implements StudentDAO {
+
+
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    private List<Student> students;
+
+
+    @Transactional
+//    @Override
+    public List<Student> getStudents() {
+
+        System.out.println("sessionFactory = " + sessionFactory);
+
+        students = (List<Student>) sessionFactory.getCurrentSession()
+                .createCriteria(Student.class)
+                .setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY).list();
+
+        System.out.println("sessionFactory = " + sessionFactory);
+
+        return students;
+    }
+
+
+
+}
--- src/main/java/com/university/dao/interfaces/StudentDAO.java
+++ src/main/java/com/university/dao/interfaces/StudentDAO.java
@@ -1,12 +1,12 @@
-package com.university.dao.interfaces;//package com.university.com.university.dao.interfaces;
-//
-//import com.university.com.university.entities.Student;
-//
-//import java.util.List;
-//
-//
-//public interface StudentDAO {
-//
-//    /*List<Student>*/ void getStudents();
-//
-//}
+package com.university.dao.interfaces;
+
+import com.university.entities.Student;
+
+import java.util.List;
+
+
+public interface StudentDAO {
+
+    List<Student> getStudents();
+
+}
