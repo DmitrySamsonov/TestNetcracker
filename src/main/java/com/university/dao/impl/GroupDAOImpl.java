6a76a30d0c95e61d8bc553675c914edf4028fc31
DmitrySamsonov <dimasamsonov7@gmail.com>
2016-12-18 01:50:32 +0300

    add GroupDAOImpl

---
+++ src/main/java/com/university/dao/impl/GroupDAOImpl.java
@@ -0,0 +1,44 @@
+package com.university.dao.impl;
+
+import com.university.dao.interfaces.GroupDAO;
+import com.university.entities.Group;
+import com.university.entities.Student;
+import org.hibernate.Criteria;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+public class GroupDAOImpl implements GroupDAO{
+
+
+    @Autowired
+    private SessionFactory sessionFactory;
+    private List<Group> groups;
+
+
+    @Transactional
+    @Override
+    public List<Group> getGroups() {
+        System.out.println("sessionFactory = " + sessionFactory);
+        try{
+            groups = (List<Group>) sessionFactory.getCurrentSession()
+                    .createCriteria(Group.class)
+                    .setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY).list();
+        }
+        catch(Exception ex){
+            System.out.println(ex);
+        }
+        finally{
+            return groups;
+        }
+    }
+
+
+
+
+
+}
---
+++ src/main/java/com/university/dao/interfaces/GroupDAO.java
@@ -0,0 +1,9 @@
+package com.university.dao.interfaces;
+
+import com.university.entities.Group;
+
+import java.util.List;
+
+public interface GroupDAO {
+    List<Group> getGroups();
+}
