b3386103250358fa7a2eb089b96066aed99accf7
DmitrySamsonov <dimasamsonov7@gmail.com>
2016-12-19 06:12:09 +0300

    add jquery validate scolarship

--- src/main/webapp/resources/js/formValidate.js
+++ src/main/webapp/resources/js/formValidate.js
@@ -3,7 +3,7 @@ $(function(){
     $("#groupNumber_error_message").hide();
     $("#scolarship_error_message").hide();
 
-    var error_fio = false;
+
     var error_groupNumber = false;
     var error_scolarship = false;
 
@@ -18,28 +18,40 @@ $(function(){
     });
 
 
-    function check_fio_length() {
+    function check_fio() {
         var fio_length = $("#form_fio").val().length;
+        var fio_english = $("#form_fio").val().match(/^[-a-zA-Z ]+$/i);
+
+        var error_fio = false;
 
         if (fio_length < 8 || fio_length > 40) {
             $("#fio_error_message").html("Should be between 8-40 characters");
             $("#fio_error_message").show();
             error_fio = true;
-        } else {
-            $("#fio_error_message").hide();
         }
-    }
-    function check_fio_symbols() {
-        var fio_english = $("#form_fio").val().match(/^[-a-zA-Z ]+$/i);
-
         if(!fio_english){
             $("#fio_error_message").html("Only A-Z a-z ");
             $("#fio_error_message").show();
             error_fio = true;
-        }else{
+        }
+        if(error_fio == false){
             $("#fio_error_message").hide();
         }
-
     }
+
+
+    function check_scolarship() {
+        var error_scolarship = false;
+
+        if ($("#form_scolarship").val() < 1.0 || $("#form_scolarship").val() > 2.0) {
+            $("#scolarship_error_message").html("Should be between 1.0 - 2.0");
+            $("#scolarship_error_message").show();
+            error_scolarship = true;
+        }
+        if(error_scolarship == false){
+            $("#scolarship_error_message").hide();
+        }
+    }
+
 
 });
